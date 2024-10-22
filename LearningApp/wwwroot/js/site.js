098acb7c123c0e17637a29d19dac591c3e824b54
Yasin Yazdan Ahmed <yazdan410@gmail.com>
2024-10-22 12:03:46 -0400

    test commit

---
+++ LearningApp/wwwroot/js/site.js
@@ -0,0 +1,4 @@
+﻿// Please see documentation at http://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
